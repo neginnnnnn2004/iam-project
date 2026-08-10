ed4f60e9e49534ac4f0eef4c60909cd4cd0be484
nilan <nilanmahdavi113@gmail.com>
2026-08-11 00:15:18 +0330

    change the url for resetPassView to: 'auth/reset_password/' and the name to: update-profile'

--- identity/urls.py
+++ identity/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('auth/register/', auth_views.UserRegisterView.as_view(), name='user-register'),
     path('auth/login/', auth_views.UserLoginView.as_view(), name='user-login'),
     path('auth/profile/update/', auth_views.ProfileUpdateView.as_view(), name='update-profile'),
-    path('auth/password-reset/', reset_pass_views.PasswordResetWithBackupCodeView.as_view(),name='password_reset_confirm'),
+    path('auth/reset_password/', reset_pass_views.PasswordResetWithBackupCodeView.as_view(),name="reset_password"),
     path('auth/myRole/', user_views.ReturnTheRoleOfUser.as_view(), name='my_role'),
 
     path('admin/list-of-users/', user_views.ListOfUsersView.as_view(), name='list-of-users'),
