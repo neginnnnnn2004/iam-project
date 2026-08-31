b11912150955084e6529b63668b1b3203dd93b42
nilan <nilanmahdavi113@gmail.com>
2026-08-31 07:22:15 +0330

    update test file

--- accounts/tests/test_reset_pass.py
+++ accounts/tests/test_reset_pass.py
@@ -758,3 +758,4 @@ class UserResetPasswordTest(APITestCase):
 
                 self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
                 self.assertEqual(response.data["error_code"], 75)
+
